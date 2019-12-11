c7da050d4472678baa9df2a9e791fb723000cadc
Quy <quynx1698@gmail.com>
2019-12-11 09:40:47 +0700

    Add middleware for user's validation

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 var controller = require("../controllers/user.controller");
+var validate = require("../validate/user.validate");
 
 router.get("/", controller.index);
 
@@ -11,6 +12,6 @@ router.get("/create", controller.create);
 
 router.get("/:id", controller.get);
 
-router.post("/create", controller.postCreate);
+router.post("/create", validate.postCreate, controller.postCreate);
 
 module.exports = router;
