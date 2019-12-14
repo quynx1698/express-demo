e0e9402c28ce2d608a2120da70dd43b1b236be00
Quy <quynx1698@gmail.com>
2019-12-14 08:21:03 +0700

    Signed cookie demo

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set("views", "./views");
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser());
+app.use(cookieParser("dasdjgfjghjg"));
 
 app.get("/", function(req, res) {
   res.render("index", {
