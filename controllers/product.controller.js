8f597b968e6f27122ed79d440ac9321c1852b5db
Quy <quynx1698@gmail.com>
2019-12-15 10:49:19 +0700

    Add pagination for products

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,21 @@ var db = require("../db");
 
 module.exports.showProductList = function(req, res, next) {
   var products = db.get("product").value();
+
+  var page = parseInt(req.query.page) || 1;
+  var perPage = 8;
+
+  var start = (page - 1) * perPage;
+  var end = page * perPage;
+
+  var drop = (page - 1) * perPage;
+
   res.render("products/index", {
-    products: products
+    //products: products.slice(start, end)
+    products: db
+      .get("product")
+      .drop(drop)
+      .take(perPage)
+      .value()
   });
 };
